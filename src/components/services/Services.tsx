b3943a4e769d1d275e329852bdab2f1f9e0081e0
shimin <mandyshimin18@gmail.com>
2024-04-12 02:59:26 -0700

    april 12

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -15,14 +15,14 @@ const Services: React.ForwardRefRenderFunction<HTMLDivElement, textType> = (text
           <div className='linefour'>Our services offer your company the solutions it needs to work more productively.</div>
         </div>
         <div className='cards'>
-         <DesignCards imageUrl={require('../../images/work1.webp')} title="Custom software development"  description="Build tailored software solutions to meet your unique business needs."/>
-         <DesignCards imageUrl={require('../../images/work2.webp')} title="Mobile app development" description="Craft innovative mobile applications for iOS and Android platforms."/>
-         <DesignCards imageUrl={require('../../images/work3.webp')} title="Web development" description="Create stunning and responsive websites with cutting-edge technologies."/>
+         <DesignCards imageUrl={require('../../images/work1.png')} title="Custom software development"  description="Build tailored software solutions to meet your unique business needs."/>
+         <DesignCards imageUrl={require('../../images/work2.png')} title="Mobile app development" description="Craft innovative mobile applications for iOS and Android platforms."/>
+         <DesignCards imageUrl={require('../../images/work3.png')} title="Web development" description="Create stunning and responsive websites with cutting-edge technologies."/>
         </div>
         <div className='cards2'>
-         <DesignCards imageUrl={require('../../images/work4.webp')} title="Cloud integration services" description="Integrate cloud solutions for seamless business operations."/>
-         <DesignCards imageUrl={require('../../images/work5.webp')} title="Quality assurance and testing" description="Ensure the reliability and performance of your software products."/>
-         <DesignCards imageUrl={require('../../images/work6.webp')} title="UX/UI design" description="Craft intuitive and visually appealing user experiences for your software products."/>
+         <DesignCards imageUrl={require('../../images/work4.png')} title="Cloud integration services" description="Integrate cloud solutions for seamless business operations."/>
+         <DesignCards imageUrl={require('../../images/work5.png')} title="Quality assurance and testing" description="Ensure the reliability and performance of your software products."/>
+         <DesignCards imageUrl={require('../../images/work6.png')} title="UX/UI design" description="Craft intuitive and visually appealing user experiences for your software products."/>
         </div>
         
       </div>
